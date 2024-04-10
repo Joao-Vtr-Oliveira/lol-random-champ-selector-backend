47c4137b3dee30931930bff2cbe2cf0cbe38d073
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-10 12:44:49 -0300

    feat: add the controller that returns all champions

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import Champion from '../models/Champion';
+import Champion from '../database/schemas/Champion';
 import { champions } from '../utils/championsList';
 import { mountData } from '../utils/mountData';
 
@@ -11,30 +11,42 @@ export const ping = (req: Request, res: Response) => {
 // ! Controller that adds the champions, will cause error
 export const addAllCharacters = async (req: Request, res: Response) => {
 	try {
-		const result = await Champion.insertMany(champions, {ordered: true});
+		const result = await Champion.insertMany(champions, { ordered: true });
 		console.log(result);
-		res.send({status: 'ok', champios: result});
+		res.send({ status: 'ok', champios: result });
 	} catch (error) {
 		console.log(error);
-		res.send({status: 'erro'})
+		res.send({ status: 'erro' });
 	}
 };
 
 // ? Controller that returns a random champion
 export const getChampion = async (req: Request, res: Response) => {
-	const {role, type, ranged} = req.body;
-	
+	const { role, type, ranged } = req.body;
+
 	try {
-		const match = mountData({role, type, ranged});
-		console.log(match)
+		const match = mountData({ role, type, ranged });
+		console.log(match);
 		const result = await Champion.aggregate([
-			{$match: match},
-			{$sample: {size: 1}}
-		])
+			{ $match: match },
+			{ $sample: { size: 1 } },
+		]);
 		console.log(result);
-		res.send({requirements: match, result});
+
+		res.send({ requirements: match, result });
 	} catch (error) {
 		console.log(error);
-		res.send({status: 'error'});
+		res.send({ status: 'error' });
 	}
-}
+};
+
+// ? Controller that returns all champions
+export const getAllChampions = async (req: Request, res: Response) => {
+	try {
+		const result = await Champion.find({});
+		res.send({ result });
+	} catch (error) {
+		console.log(error);
+		res.send({ status: 'error' });
+	}
+};
