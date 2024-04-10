11e4498dd8bd8b8d0a36ea9aded8cb765f9fd666
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-10 15:08:38 -0300

    feat: add the updateData function, that creates an object for the updated champion

---
+++ src/utils/updateData.ts
@@ -0,0 +1,43 @@
+import { ChampionQuery } from '../types/championQueryType';
+import { ChampionMountType, RolesType, TypeDamageType } from '../types/championsTypes';
+
+
+interface ChampionUpdateQuery extends ChampionQuery {
+  name: string;
+  nameBase?: string;
+}
+
+interface ChampionUpdateType extends ChampionMountType {
+  name: string;
+  nameBase?: string;
+}
+
+export const mountUpdatedData = ({name, nameBase, role, type, ranged}: ChampionUpdateQuery) => {
+	const match: ChampionUpdateType = {
+    name
+  }
+
+  if (role !== undefined) {
+    match.top = false;
+    match.jg = false;
+    match.mid = false;
+    match.adc = false;
+    match.sup = false;
+    const roleArray = role.split(',') as RolesType[];
+    for (let i = 0; i < roleArray.length; i++) {
+      match[roleArray[i]] = true;
+    }
+  };
+  if (type !== undefined) {
+    match.ad = false;
+    match.ap = false;
+    match.tank = false;
+    const typeArray = type.split(',') as TypeDamageType[];
+    for (let i = 0; i < typeArray.length; i++) {
+      match[typeArray[i]] = true;
+    }
+  };
+	if (ranged !== undefined) match.ranged = ranged === 'true' ? true : false;
+  if (nameBase !== undefined) match.nameBase = nameBase;
+	return match;
+}
