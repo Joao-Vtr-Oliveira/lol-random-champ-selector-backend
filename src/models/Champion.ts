67caac5f6d447279111f1b2189cf71c1357bee7d
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-10 11:21:01 -0300

    refactor: change the model for the new format of db

--- src/models/Champion.ts
+++ src/models/Champion.ts
@@ -1,18 +1,5 @@
 import { Schema, model, connection, Model } from 'mongoose';
-
-export type ChampionType = {
-  name: string;
-  top: boolean;
-  jg: boolean;
-  mid: boolean;
-  adc: boolean;
-  sup: boolean;
-  ad: boolean;
-  ap: boolean;
-  tank: boolean;
-  ranged: boolean;
-  nameBase: string;
-}
+import { ChampionType } from '../types/championsTypes';
 
 const schema = new Schema<ChampionType>({
   name: {type: String, required: true, unique: true},
