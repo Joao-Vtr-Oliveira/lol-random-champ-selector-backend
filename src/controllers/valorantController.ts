e9c4942b4007d0fe03576ab4c169a4fa0a5a2135
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-18 10:02:56 -0300

    feat: add the controller that adds all agents

--- src/controllers/valorantController.ts
+++ src/controllers/valorantController.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
 import Agent from '../database/schemas/Valorant-Agent';
+import { agents } from '../utils/agentList';
+
+// ! Controller that adds the agents, will cause error
+export const addAllAgents = async (req: Request, res: Response) => {
+	try {
+		const result = await Agent.insertMany(agents, { ordered: true });
+		console.log(result);
+		res.send({ status: 'ok', champios: result });
+	} catch (error) {
+		console.log(error);
+		res.send({ status: 'erro' });
+	}
+};
 
 export const getAllAgents = async (req: Request, res: Response) => {
 	try {
@@ -7,7 +20,7 @@ export const getAllAgents = async (req: Request, res: Response) => {
 		console.log('Agentes: ', agents);
 		res.send({agents});
 	} catch (error) {
-		console.log('erro')
+		console.log('error');
 		res.status(404);
 	}
 }
