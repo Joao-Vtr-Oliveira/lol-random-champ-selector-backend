e96eefdf6427f71e19afa2484835d7475e25951d
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-18 10:25:47 -0300

    feat: add the controller that returns a random agent

--- src/controllers/valorantController.ts
+++ src/controllers/valorantController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Agent from '../database/schemas/Valorant-Agent';
 import { agents } from '../utils/agentList';
+import { mountAgentData } from '../utils/mountAgentData';
 
 // ! Controller that adds the agents, will cause error
 export const addAllAgents = async (req: Request, res: Response) => {
@@ -24,3 +25,21 @@ export const getAllAgents = async (req: Request, res: Response) => {
 		res.status(404);
 	}
 }
+
+// ? Controller that returns a random champion
+export const getRandomAgent = async (req: Request, res: Response) => {
+	const { role } = req.body;
+
+	try {
+		const match = mountAgentData({ role });
+		const result = await Agent.aggregate([
+			{ $match: match },
+			{ $sample: { size: 1 } },
+		]);
+		if(result.length > 0) return res.send({ requirements: match, result });
+		res.status(404).send({requirements: match, status: 'There is none agent with your requirements.'});
+	} catch (error) {
+		console.log(error);
+		res.status(400).send({ status: 'error' });
+	}
+};
