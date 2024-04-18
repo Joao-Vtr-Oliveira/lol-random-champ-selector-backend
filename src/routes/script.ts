be9c570995e07767b65d90daf0e0ed48eea63454
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-18 09:47:18 -0300

    feat: add the route that shows every valorant agent

--- src/routes/script.ts
+++ src/routes/script.ts
@@ -1,29 +1,33 @@
 import { Router } from 'express';
 import * as PageController from '../controllers/pageController';
+import * as lolController from '../controllers/lolController';
+import * as valorantController from '../controllers/valorantController';
 
 const router = Router();
 
 router.get('/ping', PageController.ping);
 
 // ! Only use once.
-router.post('/addAllCharacters', PageController.addAllCharacters);
+router.post('/addAllCharacters', lolController.addAllCharacters);
 
 // Route that returns a random champ with the sent data
-router.post('/getRandomChampion', PageController.getRandomChampion);
+router.post('/getRandomChampion', lolController.getRandomChampion);
 
 // Route that returns all champions
-router.get('/getAllChampions', PageController.getAllChampions);
+router.get('/getAllChampions', lolController.getAllChampions);
 
 // Route that returns a SPECIF champion selected by name.
-router.post('/getSpecificChampion', PageController.getSpecificChampion);
+router.post('/getSpecificChampion', lolController.getSpecificChampion);
 
 // Route that adds a new champion
-router.post('/addNewChampion', PageController.addNewChampion);
+router.post('/addNewChampion', lolController.addNewChampion);
 
 // Route that deletes a champion
-router.delete('/deleteChampion', PageController.deleteChampion);      
+router.delete('/deleteChampion', lolController.deleteChampion);      
 
 // Route that updates a champion
-router.put('/updateChampion', PageController.updateChampion);
+router.put('/updateChampion', lolController.updateChampion);
+
+router.get('/valorant', valorantController.getAllAgents);
 
 export default router;
