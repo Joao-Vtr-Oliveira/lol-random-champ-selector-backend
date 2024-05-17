dea1a1e04dee820453d29f10dcffcf2d420b81a4
João V <joao.vtr.oliveira@outlook.com>
2024-05-17 15:34:23 -0300

    fix: add a variable that if the function has to add the champions or not

--- src/utils/addAllCharacters.ts
+++ src/utils/addAllCharacters.ts
@@ -3,6 +3,8 @@ import { champions } from './championsList';
 
 export const addAllCharacters = async () => {
   try {
+    const info = await Champion.findOne({name: 'Bard'});
+    if(info) return console.log('Champions already exists.');
 		const result = await Champion.insertMany(champions, { ordered: true });
 		console.log(result);
 	} catch (error) {
