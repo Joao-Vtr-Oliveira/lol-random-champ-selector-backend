5e1e643e66338ee77f1edb550a4af0a7c9aeb725
João V <joao.vtr.oliveira@outlook.com>
2024-05-17 15:37:32 -0300

    chores: change the function name and return the result

--- src/utils/addAllCharacters.ts
+++ src/utils/addAllCharacters.ts
@@ -1,12 +1,13 @@
 import Champion from '../database/schemas/Champion';
 import { champions } from './championsList';
 
-export const addAllCharacters = async () => {
+export const addAllCharactersFunction = async () => {
   try {
     const info = await Champion.findOne({name: 'Bard'});
     if(info) return console.log('Champions already exists.');
 		const result = await Champion.insertMany(champions, { ordered: true });
 		console.log(result);
+    return result;
 	} catch (error) {
 		console.log(error);
 	}
